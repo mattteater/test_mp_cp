1c8d4f42f2e7bb4a5ca2dadb719109f81d7cf890
matteater <85717822+matteater@users.noreply.github.com>
2023-10-07 08:49:37 -0400

    Seperated py and js code.

--- main.py
+++ main.py
@@ -6,18 +6,3 @@
 #     print('for git commit bug')
 
 # main()
-
-# Useful Refs.
-
-# ** JS **
-
-# var arr = [ {"id":"10", "class": "child-of-9"}, {"id":"11", "class": "child-of-10"}];
-    
-# for (var i = 0; i < arr.length; i++){
-#   document.write("<br><br>array index: " + i);
-#   var obj = arr[i];
-#   for (var key in obj){
-#     var value = obj[key];
-#     document.write("<br> - " + key + ": " + value);
-#   }
-# }
